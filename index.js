3e2a1c02a039403ef1ed359017d537611962784c
Albertyson <4lbertyson@gmail.com>
2015-11-30 00:08:50 -0600

    cadenas aceptadas

--- index.js
+++ index.js
@@ -2,27 +2,118 @@ $(document).ready(function(){
 	var ctrl=this;
 	var chars=[];
 	var position=0;
+	var estadoActual="q0"
+	var cadenaManejada="";
+	var automata={
+		"q0":{
+			"0":["X","R","q1"],
+			"1":["X","R","q2"],
+			"2":["X","R","q3"],
+			"#":["#","R","q8"]
+		},
+		"q1":{
+			"0":["0","R","q1"],
+			"1":["1","R","q1"],
+			"2":["2","R","q1"],
+			"#":["#","R","q4"]
+		},
+		"q2":{
+			"0":["0","R","q2"],
+			"1":["1","R","q2"],
+			"2":["2","R","q2"],
+			"#":["#","R","q6"]
+		},
+		"q3":{
+			"0":["0","R","q3"],
+			"1":["1","R","q3"],
+			"2":["2","R","q3"],
+			"#":["#","R","q7"]
+		},
+		"q4":{
+			"Y":["Y","R","q4"],
+			"0":["Y","L","q5"]
+		},
+		"q5":{
+			"0":["0","L","q5"],
+			"1":["1","L","q5"],
+			"2":["2","L","q5"],
+			"#":["#","L","q5"],
+			"Y":["Y","L","q5"],
+			"X":["X","R","q0"]
+		},
+		"q6":{
+			"Y":["Y","R","q6"],
+			"1":["Y","L","q5"]
+		},
+		"q7":{
+			"Y":["Y","R","q7"],
+			"2":["Y","L","q5"]
+		},
+		"q8":{
+			"Y":["Y","R","q8"],
+			" ":[" ","S","q9"],
+		},
+		"q9":{
+		}
+	}
+
 	function delta(estado,elementoCinta){
 		var retVal={};
-
+		console.log("estado: ",estado);
+		console.log("símbolo: ",elementoCinta);
+		retVal=automata[estado][elementoCinta];
+		//console.log("Escribe",retVal[0]);
+		//console.log("Direccion",retVal[1]);
+		//console.log("Siguiente Estado",retVal[2]);
+		return retVal;
 	}
 	function buscar(){
-		paintBox(position);
+		position=0;
+		estadoActual="q0";
+		cadenaManejada=$("#cadena").val()
+		drawBoxes(cadenaManejada,position);
+	}
+	function siguiente(){
+		resDelta=delta(estadoActual,cadenaManejada.split("")[position]==undefined?" ":cadenaManejada.split("")[position]);
+		if(resDelta==undefined){
+			alert("Cadena no aceptada");
+			buscar();
+			return;
+		}
+		var aEscribir=resDelta[0];
+		var direccion=resDelta[1];
+		var siguienteEstado=resDelta[2];
+		if(siguienteEstado=="q9"){
+			alert("Cadena aceptada");
+			buscar();
+			return;
+		}
+		var temp=cadenaManejada.split("");
+		temp[position]=aEscribir;
+		cadenaManejada=temp.join("");
+		position=direccion=="R"?position+1:position-1;
+		estadoActual=siguienteEstado;
+		drawBoxes(cadenaManejada,position);
+	}
+	function direccion(){
 
 	}
 	
-	function drawBoxes(cadena){
+	function drawBoxes(cadena,positionMark){
 		chars=cadena.split("");
-		var firstHalf=chars.splice(0,chars.length/2);
-		var temp=chars.splice(0,chars.length);
-		chars=[];
-		chars=chars.concat(firstHalf);
-		chars=chars.concat(["#"]);
-		chars=chars.concat(temp);
+		if(chars.indexOf("#")==-1){
+			var firstHalf=chars.splice(0,chars.length/2);
+			var temp=chars.splice(0,chars.length);
+			chars=[];
+			chars=chars.concat(firstHalf);
+			chars=chars.concat(["#"]);
+			chars=chars.concat(temp);
+		}
+		cadenaManejada=chars.join("");
 		var jsonRectangles=[];
 		var jsonTexts=[];
 		d3.select("#CadenaDiv").html("");
-		var svgContainer = d3.select("#CadenaDiv").append("svg").attr("width", 600).attr("height", 600);
+		var svgContainer = d3.select("#CadenaDiv").append("svg").attr("width", "100%").attr("height", 600);
 
 		for(var idx=0;idx<chars.length;idx++){
 			var val=chars[idx];
@@ -33,7 +124,7 @@ $(document).ready(function(){
 				"width": 50,
 				"stroke": "black",
 				"stroke-width": 1,
-				"color":"white"
+				"color":positionMark==idx?"yellow":"white"
 			})
 			jsonTexts.push({
 				"x_axis": 50*(idx+1)+15,
@@ -74,16 +165,13 @@ $(document).ready(function(){
 			.style("fill", function(d) { return d.color; })
 	}
 
-	function paintBox(position){
-		
-	}
 	function init(){
 		$("#go").bind("click",buscar);
 		$("#cadena").keyup(function(event){
 			var cadena=$("#cadena").val();
 			drawBoxes(cadena);
 		});
-		var svgContainer = d3.select("#rightDiv").append("svg").attr("width", 200).attr("height", 200);
+		$("#nextButton").bind("click",siguiente);
 	};
 	init();
 })
